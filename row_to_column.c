60059507b541fc9082b58ef7de2b07d84b08fbe3
Vignesh G <115526040+vignesh496@users.noreply.github.com>
2026-01-23 18:02:26 +0530

    Update row_to_column.c

--- row_to_column.c
+++ row_to_column.c
@@ -1,53 +1,33 @@
 #include "postgres.h"
 #include "fmgr.h"
 #include "miscadmin.h"
-#include "utils/memutils.h"
 
 #include "postmaster/bgworker.h"
-#include "storage/ipc.h"
 #include "storage/latch.h"
+#include "storage/ipc.h"
 
 #include "executor/spi.h"
+#include "utils/memutils.h"
 #include "utils/builtins.h"
-#include "utils/guc.h"
 #include "utils/snapmgr.h"
+
+#include "lib/stringinfo.h"
+#include "libpq/pqformat.h"          
+#include "replication/logicalproto.h"
+
+#include "portability/instr_time.h"
+#include "utils/timestamp.h"
+
 
 PG_MODULE_MAGIC;
 
-typedef struct HtapTxn
-{
-    TransactionId xid;
-    List *sql_list;   /* list of char* SQL strings */
-} HtapTxn;
-
-static HtapTxn *current_txn = NULL;
-
-
-/* Signal flags */
-static volatile sig_atomic_t got_sighup = false;
+/* ------------------------------------------------------------
+ * SIGNAL
+ * ------------------------------------------------------------*/
 static volatile sig_atomic_t got_sigterm = false;
-static void log_sql_from_decoded_change(const char *data); 
-// static void buffer_insert(const char *table, const char *data);
-// static void buffer_delete(const char *table, const char *data);
-
-
-extern HtapTxn *current_txn;
-
-void _PG_init(void);
-PGDLLEXPORT void row_to_column_main(Datum main_arg) pg_attribute_noreturn();
-
-/* Signal handlers */
-static void
-row_to_column_sighup(SIGNAL_ARGS)
-{
-    int save_errno = errno;
-    got_sighup = true;
-    SetLatch(MyLatch);
-    errno = save_errno;
-}
 
 static void
-row_to_column_sigterm(SIGNAL_ARGS)
+handle_sigterm(SIGNAL_ARGS)
 {
     int save_errno = errno;
     got_sigterm = true;
@@ -55,236 +35,260 @@ row_to_column_sigterm(SIGNAL_ARGS)
     errno = save_errno;
 }
 
-PGDLLEXPORT void
-row_to_column_main(Datum main_arg)
+/* ------------------------------------------------------------
+ * TRANSACTION BUFFER
+ * ------------------------------------------------------------*/
+typedef struct
 {
-    pqsignal(SIGHUP, row_to_column_sighup);
-    pqsignal(SIGTERM, row_to_column_sigterm);
-    BackgroundWorkerUnblockSignals();
+    List *sql_list;
+} TxnBuffer;
 
-    /* Connect to target DB */
-    BackgroundWorkerInitializeConnection("postgres", NULL, 0);
+static TxnBuffer *current_txn = NULL;
 
-    ereport(LOG,
-        (errmsg("HTAP bgworker started (SQL logical decoding)")));
+/* ------------------------------------------------------------
+ * RELATION CACHE
+ * ------------------------------------------------------------*/
+#define MAX_COLS 64
 
-    while (!got_sigterm)
+typedef struct
+{
+    Oid     relid;
+    char    relname[NAMEDATALEN];
+    int     ncols;
+    Oid     coltypes[MAX_COLS];
+} RelationCache;
+
+static RelationCache relcache;
+
+/* ------------------------------------------------------------
+ * HELPERS
+ * ------------------------------------------------------------*/
+static bool
+needs_quotes(Oid typid)
+{
+    switch (typid)
     {
-        int rc;
-
-        rc = WaitLatch(
-            MyLatch,
-            WL_LATCH_SET | WL_TIMEOUT | WL_POSTMASTER_DEATH,
-            1L,
-            0);
-
-        ResetLatch(MyLatch);
-
-        if (rc & WL_POSTMASTER_DEATH)
-            proc_exit(1);
-
-        if (got_sighup)
-        {
-            got_sighup = false;
-            ProcessConfigFile(PGC_SIGHUP);
-        }
-
-        CHECK_FOR_INTERRUPTS();
-
-        /* -------- LOGICAL DECODING -------- */
-
-        StartTransactionCommand();
-
-        /* REQUIRED in PG17 */
-        PushActiveSnapshot(GetTransactionSnapshot());
-
-        SPI_connect();
-
-        int ret = SPI_execute(
-            "SELECT lsn, xid, data "
-            "FROM pg_logical_slot_get_changes('htap_sync', NULL, NULL)",
-            true,
-            0);
-            if (SPI_processed > 0)
-                ereport(LOG, (errmsg("[HTAP] decoded %lu rows", SPI_processed)));
-
-
-        if (ret == SPI_OK_SELECT)
-        {
-            for (uint64 i = 0; i < SPI_processed; i++)
-            {
-                char *lsn =
-                    SPI_getvalue(SPI_tuptable->vals[i],
-                                SPI_tuptable->tupdesc, 1);
-                char *xid =
-                    SPI_getvalue(SPI_tuptable->vals[i],
-                                SPI_tuptable->tupdesc, 2);
-                char *data =
-                    SPI_getvalue(SPI_tuptable->vals[i],
-                                SPI_tuptable->tupdesc, 3);
-
-                if (data)
-                {
-                    ereport(LOG,
-                        (errmsg("[HTAP-DECODE] lsn=%s xid=%s %s",
-                                lsn ? lsn : "NULL",
-                                xid ? xid : "NULL",
-                                data)));
-
-                    log_sql_from_decoded_change(data);
-                }
-            }
-        }
-
-        SPI_finish();
-
-        /* REQUIRED cleanup */
-        PopActiveSnapshot();
-        CommitTransactionCommand();
-        /* ---------------------------------- */
+        case INT2OID:
+        case INT4OID:
+        case INT8OID:
+        case FLOAT4OID:
+        case FLOAT8OID:
+            return false;
     }
-
-    ereport(LOG, (errmsg("HTAP bgworker shutting down")));
-    proc_exit(0);
+    return true;
 }
 
 static void
-log_sql_from_decoded_change(const char *data)
+buffer_sql(const char *sql)
 {
-    char table[128];
-    char col_table[160];
-    const char *p;
+    if (!current_txn)
+        elog(ERROR, "buffer_sql outside transaction");
 
-    StringInfoData columns;
-    StringInfoData values;
-    StringInfoData sql;
+    current_txn->sql_list =
+        lappend(current_txn->sql_list, pstrdup(sql));
+}
+
+/* 
+ * ------------------------------------------------------------
+ * PGOUTPUT DECODER (PG17 SAFE)
+ * ------------------------------------------------------------
+*/
+
+static void
+decode_pgoutput(bytea *data)
+{
+    StringInfoData msg;
+
+    /* Correct initialization */
+    msg.data   = VARDATA(data);
+    msg.len    = VARSIZE(data) - VARHDRSZ;
+    msg.maxlen = msg.len;
+    msg.cursor = 0;
+
+    char tag = pq_getmsgbyte(&msg);
 
     /* ---------- BEGIN ---------- */
-    if (strncmp(data, "BEGIN", 5) == 0)
+    if (tag == 'B')
     {
-        if (current_txn)
-            return;
-
-        current_txn = palloc0(sizeof(HtapTxn));
-        current_txn->xid = GetCurrentTransactionIdIfAny();
-        current_txn->sql_list = NIL;
-
-        ereport(DEBUG1, (errmsg("[HTAP] BEGIN txn")));
+        current_txn = palloc0(sizeof(TxnBuffer));
         return;
     }
 
     /* ---------- COMMIT ---------- */
-    if (strncmp(data, "COMMIT", 6) == 0)
+    if (tag == 'C')
     {
-        if (!current_txn)
-            return;
+        ListCell   *lc;
+        instr_time  start, duration;
+        uint64      row_count = list_length(current_txn->sql_list);
 
-        ereport(LOG,
-            (errmsg("[HTAP] COMMIT txn, applying %d changes",
-                    list_length(current_txn->sql_list))));
+        INSTR_TIME_SET_CURRENT(start);
 
-        ListCell *lc;
         foreach (lc, current_txn->sql_list)
-        {
-            char *sqlstr = (char *) lfirst(lc);
+            SPI_execute(lfirst(lc), false, 0);
 
-            int rc = SPI_execute(sqlstr, false, 0);
-            if (rc < 0)
-                ereport(WARNING,
-                    (errmsg("[HTAP] SPI rc=%d for: %s", rc, sqlstr)));
-        }
+        INSTR_TIME_SET_CURRENT(duration);
+        INSTR_TIME_SUBTRACT(duration, start);
+
+        /* Log performance data */
+        elog(LOG, "[PERF] Columnar Sync: processed %lu rows in %.3f ms (Avg: %.4f ms/row)",
+            row_count,
+            INSTR_TIME_GET_MILLISEC(duration),
+            INSTR_TIME_GET_MILLISEC(duration) / (row_count ? row_count : 1));
 
         list_free_deep(current_txn->sql_list);
         pfree(current_txn);
         current_txn = NULL;
-
-        ereport(DEBUG1, (errmsg("[HTAP] COMMIT txn applied")));
         return;
     }
 
-    /* ---------- MUST BE IN TXN ---------- */
-    if (!current_txn)
-        return;
-
-    /* ---------- FILTER TABLE ---------- */
-    if (!strstr(data, "table public."))
-        return;
-
-    if (sscanf(data, "table %*[^.].%127[^:]:", table) != 1)
-        return;
-
-    snprintf(col_table, sizeof(col_table), "%s_col", table);
-
-    // INSERT ONLY (UNCHANGED)
-
-    if ((p = strstr(data, "INSERT:")) == NULL)
-        return;
-
-    initStringInfo(&columns);
-    initStringInfo(&values);
-    initStringInfo(&sql);
-
-    p += strlen("INSERT:");
-    bool first = true;
-
-    while (*p)
+    /* ---------- RELATION ---------- */
+    if (tag == 'R')
     {
-        char col[64];
-        char val[256];
+        relcache.relid = pq_getmsgint(&msg, 4);
 
-        if (sscanf(p, " %63[^[][%*[^]]]:%255s", col, val) != 2)
-            break;
+        pq_getmsgstring(&msg); /* namespace */
 
-        if (!first)
+        strlcpy(relcache.relname,
+                pq_getmsgstring(&msg),
+                NAMEDATALEN);
+
+        pq_getmsgbyte(&msg); /* replica identity */
+
+        relcache.ncols = pq_getmsgint(&msg, 2);
+
+        for (int i = 0; i < relcache.ncols; i++)
         {
-            appendStringInfoString(&columns, ", ");
-            appendStringInfoString(&values, ", ");
+            pq_getmsgbyte(&msg);        /* key flag */
+            pq_getmsgstring(&msg);      /* column name */
+            relcache.coltypes[i] = pq_getmsgint(&msg, 4);
+            pq_getmsgint(&msg, 4);      /* typmod */
+        }
+        return;
+    }
+
+    /* ---------- INSERT ---------- */
+    if (tag == 'I')
+    {
+        pq_getmsgint(&msg, 4); /* relid */
+
+        if (pq_getmsgbyte(&msg) != 'N')
+            elog(ERROR, "expected new tuple");
+
+        int ncols = pq_getmsgint(&msg, 2);
+
+        StringInfoData sql;
+        initStringInfo(&sql);
+
+        appendStringInfo(&sql,
+                         "INSERT INTO %s_col VALUES (",
+                         relcache.relname);
+
+        for (int i = 0; i < ncols; i++)
+        {
+            char kind = pq_getmsgbyte(&msg);
+
+            if (kind == 'n')
+            {
+                appendStringInfoString(&sql, "NULL");
+            }
+            else if (kind == 't')
+            {
+                int len = pq_getmsgint(&msg, 4);
+                const char *val = pq_getmsgbytes(&msg, len);
+                const char *q = needs_quotes(relcache.coltypes[i]) ? "'" : "";
+
+                appendStringInfo(&sql, "%s%.*s%s",
+                                 q, len, val, q);
+            }
+            else
+                elog(ERROR, "unknown tuple kind %c", kind);
+
+            if (i < ncols - 1)
+                appendStringInfoString(&sql, ", ");
         }
 
-        appendStringInfoString(&columns, col);
+        appendStringInfoString(&sql, ");");
 
-        if (strcmp(val, "NULL") == 0)
-            appendStringInfoString(&values, "NULL");
-        else
-            appendStringInfo(&values, "%s", val);
-
-        first = false;
-
-        p = strchr(p + 1, ' ');
-        if (!p)
-            break;
+        buffer_sql(sql.data);
+        pfree(sql.data);
     }
-
-    appendStringInfo(&sql,
-        "INSERT INTO %s (%s) VALUES (%s);",
-        col_table, columns.data, values.data);
-
-    current_txn->sql_list =
-        lappend(current_txn->sql_list, pstrdup(sql.data));
-
-    ereport(DEBUG1, (errmsg("[HTAP-BUFFER INSERT] %s", sql.data)));
-
-    pfree(columns.data);
-    pfree(values.data);
-    pfree(sql.data);
 }
 
+/* ------------------------------------------------------------
+ * BGWORKER MAIN
+ * ------------------------------------------------------------*/
+PGDLLEXPORT void
+row_to_column_main(Datum arg)
+{
+    pqsignal(SIGTERM, handle_sigterm);
+    BackgroundWorkerUnblockSignals();
+
+    BackgroundWorkerInitializeConnection("postgres", NULL, 0);
+
+    while (!got_sigterm)
+    {
+        StartTransactionCommand();
+        PushActiveSnapshot(GetTransactionSnapshot());
+        SPI_connect();
+
+        
+        // /* Corrected SPI_execute call */
+        // SPI_execute("select data from pg_logical_slot_get_binary_changes('sample_slot2', null, null, 'proto_version', '2', 'publication_names', 'pub_sales', 'streaming', 'on')", true, 0);
+
+        /* Corrected SPI_execute call --> with committed transactions only */
+        SPI_execute("select data from pg_logical_slot_get_binary_changes('sample_slot2', null, null, 'proto_version', '1', 'publication_names', 'pub_sales')", true, 0);
+
+        if (SPI_processed == 0)
+        {
+            WaitLatch(MyLatch,
+                    WL_LATCH_SET | WL_TIMEOUT | WL_POSTMASTER_DEATH,
+                    50L,  
+                    0);
+            ResetLatch(MyLatch);
+        }
+        else
+        {
+            for (uint64 i = 0; i < SPI_processed; i++)
+            {
+                bool isnull;
+                Datum d = SPI_getbinval(
+                    SPI_tuptable->vals[i],
+                    SPI_tuptable->tupdesc,
+                    1,
+                    &isnull);
+
+                decode_pgoutput(DatumGetByteaP(d));
+            }
+        }
+
+
+        SPI_finish();
+        PopActiveSnapshot();
+        CommitTransactionCommand();
+    }
+
+    proc_exit(0);
+}
+
+/* ------------------------------------------------------------
+ * INIT
+ * ------------------------------------------------------------*/
 void
 _PG_init(void)
 {
     BackgroundWorker worker;
 
-    memset(&worker, 0, sizeof(worker));
+    MemSet(&worker, 0, sizeof(worker));
 
     worker.bgw_flags =
         BGWORKER_SHMEM_ACCESS |
         BGWORKER_BACKEND_DATABASE_CONNECTION;
 
     worker.bgw_start_time = BgWorkerStart_ConsistentState;
-    worker.bgw_restart_time = 10;
+    worker.bgw_restart_time = 5;
 
     snprintf(worker.bgw_name, BGW_MAXLEN,
-             "row_to_column_htap_worker");
+             "row_to_column_pgoutput");
 
     snprintf(worker.bgw_library_name, BGW_MAXLEN,
              "row_to_column");
@@ -292,10 +296,21 @@ _PG_init(void)
     snprintf(worker.bgw_function_name, BGW_MAXLEN,
              "row_to_column_main");
 
-    worker.bgw_main_arg = (Datum) 0;
-    worker.bgw_notify_pid = 0;
-
     RegisterBackgroundWorker(&worker);
 }
+
+
+
+
+
+// 2026-01-23 12:46:59.377 IST [96048] LOG:  [PERF] Columnar Sync: processed 100000 rows in 4942.416 ms (Avg: 0.0494 ms/row)
+// 2026-01-23 12:46:59.416 IST [96048] LOG:  starting logical decoding for slot "sample_slot2"
+
+
+// 2026-01-23 12:49:05.952 IST [96048] LOG:  [PERF] Columnar Sync: processed 1000000 rows in 41021.798 ms (Avg: 0.0410 ms/row)
+// 2026-01-23 12:49:06.110 IST [96048] LOG:  starting logical decoding for slot "sample_slot2"
+
+// 2026-01-23 13:07:21.459 IST [96048] LOG:  [PERF] Columnar Sync: processed 10000000 rows in 399520.248 ms (Avg: 0.0400 ms/row)
+// 2026-01-23 13:07:22.569 IST [96048] LOG:  starting logical decoding for slot "sample_slot2"
 
 
